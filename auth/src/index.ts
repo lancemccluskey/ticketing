9546a95eaa831c861580985023b5ef1bd00c224a
Lance McCluskey <t.lance.mccluskey@gmail.com>
2020-09-04 21:08:06 -0500

    I hope at least some of the tests pass

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  // New commmennntttssss
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!');
   }
